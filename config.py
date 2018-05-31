5289a9828dabf3837ce0b84bee0fa2e01f2ecdbd
Javier Angulo Lucerón <jangulo@tuenti.com>
2018-05-31 17:19:42 +0200

    la

--- config.py
+++
@@ -1,6 +0,0 @@
-OAUTH_CLIENT_ID = "120723272067.373244077970"
-OAUTH_SCOPE = "chat:write:user,channels:read,groups:read,im:read,channels:write,groups:write,im:write,mpim:read,mpim:write"
-OAUTH_REDIRECT_URI = "https://tuenti-assistant.appspot.com/auth/"
-OAUTH_CALLBACK_URI = "https://tuenti-assistant.appspot.com/auth/callback"
-OAUTH_CLIENT_SECRET = "d7d3d3e2e9b2a02830d535af2ca6ae45"
-CONFIGURE_URI = "https://tuenti-assistant.appspot.com/configure?session_id={session_id}"
