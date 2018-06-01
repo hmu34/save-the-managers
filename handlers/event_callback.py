deccbd2435ac568d78b2f465eec526fbbb788aec
Javier Angulo Lucerón <jangulo@tuenti.com>
2018-06-01 10:15:58 +0200

    implemented configure link reply

--- handlers/event_callback.py
+++ handlers/event_callback.py
@@ -1,9 +1,23 @@
-import logging
-
 from base_handler import BaseHandler
+from slack_api import SlackApi
+import config
 
 
 class EventCallbackHandler(BaseHandler):
+    GREETINGS = ["hola", "hello"]
+
     def handle(self, event):
-        logging.warn(event)
+        payload = event['event']
+        if payload['type'] == 'message':
+            if payload.get('text') in self.GREETINGS and\
+                    config.BOT_USER in event['authed_users'] and\
+                    payload.get('channel_type') == 'im':
+
+                channel_info = SlackApi().get_channel_info(
+                    config.BOT_ACCESS_TOKEN,
+                    payload.get('channel'))
+
+                message = "Hi, please go here to configure your account: {url}".format(url=config.AUTH_URI)
+                SlackApi().send_message(config.BOT_ACCESS_TOKEN, channel_info['id'], message, False)
+
         return self.response_ok_raw("Good!")
--- slack_api.py
+++ slack_api.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from google.appengine.api import urlfetch
 
 
@@ -10,7 +11,29 @@ class SlackApi:
             "users.profile.get",
             token,
             "user={user}".format(user=user_id)
-        ).get('profile')
+        ).get('profile', {})
+
+    def get_channel_info(self, token, channel):
+        return self._do_request(
+            "conversations.info",
+            token,
+            "channel={channel}".format(channel=channel)
+        ).get('channel', {})
+
+    def get_im_list(self, token):
+        return self._do_request(
+            "im.list",
+            token).get('ims', [])
+
+    def send_message(self, token, channel, message, as_user):
+        return self._do_request(
+            "chat.postMessage",
+            token,
+            "channel={channel}&text={message}&as_user={as_user}".format(
+                channel=channel,
+                message=message,
+                as_user=("true" if as_user else "false"))
+        )
 
     def _do_request(self, path, token, payload=None):
         result = urlfetch.fetch(
@@ -19,4 +42,5 @@ class SlackApi:
             method=urlfetch.POST,
             headers={"Authorization": "Bearer " + token}
         )
+
         return json.loads(result.content)
